8eb911a115852d632ae7e80195ef94c7aa1531be
Grace. W <grace.xuer.wu@gmail.com>
2022-01-29 20:18:03 +0800

    Hongpao

--- src/components/RedPacket.js
+++ src/components/RedPacket.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "../styles/RedPacket.css";
-
+import hongPao from "../images/HongPao.png";
 
 export default function RedPacket() {
-    return (
-        <div>Hi</div>
-    )
-};
+  return (
+    <div>
+      <img src={hongPao} className="hongPao" alt="Red packet" />
+      <h1>You are born in the year of the tiger</h1>
+    </div>
+
+  );
+}
