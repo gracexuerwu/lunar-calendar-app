381095fac9a6e0e2dd193dc335e2df9b2eadf32a
Grace. W <grace.xuer.wu@gmail.com>
2022-02-01 16:38:20 +0100

    Placed zodiac and calender text

--- src/components/RedPacket.js
+++ src/components/RedPacket.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "../styles/RedPacket.css";
 import hongPao from "../images/HongPao.png";
+import tiger from "../images/tiger.png";
 
 export default function RedPacket() {
   return (
-    <div>
-      <div className="box">
-        <img src={hongPao} className="hongPao" alt="Red packet" />
-        <div className="text">
-          <h1>You are born in the year of the Tiger 🐯</h1>
-        </div>
+    <div className="box">
+      <img src={hongPao} className="hongPao" alt="Red packet" />
+      <div className="text">
+        <h1>You are born in the year of the Tiger 🐯</h1>
+      </div>
+      <img src={tiger} className="zodiac" alt="Red packet" />
+      <div className="calendar">
+        <h2>Check out your birthday in the Chinese Lunar Calendar 👇🏻</h2>
       </div>
     </div>
   );
